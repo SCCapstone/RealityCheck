9d3c894c8d438ea4a763332085a6e2f6a4ed06cd
Ryan Shealey <shealeycr@yahoo.com>
2017-09-05 09:59:49 -0400

    Added my work towards the project so far

---
+++ Assets/Job.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Job : ThreadedJob
+{
+	protected override void ThreadFunction()
+	{
+		
+	}
+
+	protected override void OnFinished()
+	{
+		
+	}
+}
