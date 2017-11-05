a6c18265076c3f1b5ad3663bf583726739f631d9
Andrew Rowles <andrew@rowles.io>
2017-11-05 14:22:40 -0500

    simple search web app

--- webapp/app.py
+++ webapp/app.py
@@ -1,28 +1,55 @@
-from flask import Flask, request
-#import generated.search_pb2 as searchpb
+from flask import Flask, request, send_file
+import generated.search_pb2 as searchpb
+from search import DocumentFactory
 from search.indexer import IndexService
 from search.searcher import SearchService
 import lucene
+import json
 
 lucene.initVM()
 app = Flask(__name__)
 searcher = SearchService()
 
+import logging
+log = logging.getLogger("my-logger")
 
-@app.route("/search", methods=['PUT'])
+@app.route('/api/v1/search', methods=['POST'])
 def search():
     if request.headers['Content-Type'] == 'application/x-protobuf':
-        pass#search_request = searchpb.SearchRequest.ParseFromString(request.stream.read())
-        #search_result = searcher.search(search_request)
-        #return search_result.SerializeToString()
+        payload = request.stream.read()
+        search_request = searchpb.SearchRequest()
+        search_request.ParseFromString(payload)
+        search_result = searcher.search(search_request)
+        pb_out = search_result.SerializeToString()
+        return pb_out
     elif request.headers['Content-Type'] == 'application/json':
-        pass
+        return None
+
+
+@app.route('/files/<string:fname>')
+def return_files_tut(fname: str):
+    try:
+        return send_file('models/%s' % fname,
+                         attachment_filename=fname)
+    except Exception as e:
+        return str(e)
+
+
+def load_docs() -> list:
+    d = []
+    with open('docs.json') as docs_file:
+        docs = json.load(docs_file)
+        for doc in docs['models']:
+            print(doc)
+            d.append(doc)
+    return d
 
 
 if __name__=='__main__':
     indexer = IndexService()
     indexer.open("test.index-dir")
-    indexer.add_documents()
+    docs = map(lambda d: DocumentFactory.from_dict(d), load_docs())
+    indexer.add_documents(docs)
     indexer.commit()
     indexer.close()
 
