0bf4e2281967815b68502fe41ae80c54bdff6e03
NATTAPON DONRATANAPAT <pleuk5667@gmail.com>
2017-11-23 15:49:09 -0500

    add player
    I Added player, change the plane but still have a teleport error. I get too dizzy

---
+++ unity3d/Assets/Scrips/Grab.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+    public OVRInput.Controller controller;
+    public string buttonName;
+    private GameObject grabbingObject;
+    private bool grabbing = false;
+    public float grabRadius;
+    public LayerMask grabMask;
+
+    void GrabObject()
+    {
+        grabbing = true;
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabMask);
+
+        /*Hit something grab object*/
+        if(hits.Length > 0)
+        {
+            int closestHit = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].distance < hits[closestHit].distance)
+                    closestHit = i;
+            }
+            grabbingObject = hits[closestHit].transform.gameObject;
+            grabbingObject.GetComponent<Rigidbody>().isKinematic = true;
+            grabbingObject.transform.position = transform.position;
+            grabbingObject.transform.parent = transform;
+        }
+    }
+
+    void DropObject()
+    {
+        grabbing = false;
+        if(grabbingObject != null)
+        {
+            grabbingObject.transform.parent = null;
+            grabbingObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabbingObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller);
+            grabbingObject.GetComponent<Rigidbody>().angularVelocity = OVRInput.GetLocalControllerAngularVelocity(controller);
+            grabbingObject = null;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		if(!grabbing && Input.GetAxis(buttonName) == 1)
+        {
+            print("asd");
+            GrabObject();
+        }
+        if (grabbing && Input.GetAxis(buttonName) < 1)
+        {
+            DropObject();
+        }
+
+    }
+}
---
+++ unity3d/Assets/Scrips/teleporter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporter : MonoBehaviour {
+    public GameObject TeleportMarker;
+    public OVRInput.Controller controller;
+    public string buttonTeleport;
+    public string buttonRingEnable;
+    private bool isRingEnable = false;
+    public Transform Player;
+    public float RayLenght = 50;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;       
+       
+          
+        if (Physics.Raycast(ray, out hit, RayLenght))
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                if (!TeleportMarker.activeSelf)
+                    TeleportMarker.SetActive(true);
+                TeleportMarker.transform.position = hit.point;
+                if (Input.GetAxis(buttonTeleport) > 0)
+                {
+                    Teleport();
+                }
+            }
+            else
+            {
+                TeleportMarker.SetActive(false);
+            }
+                
+            
+        }
+        else
+        {
+            TeleportMarker.SetActive(false);
+        }
+        
+	}
+
+    void Teleport()
+    { 
+        Vector3 markerPosition = TeleportMarker.transform.position;
+        Player.position = new Vector3(markerPosition.x, Player.position.y, markerPosition.z);
+    }
+}
