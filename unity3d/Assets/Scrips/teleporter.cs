e73653a4b1179fa14fc8979925114213e00f9720
NATTAPON DONRATANAPAT <pleuk5667@gmail.com>
2017-11-23 20:40:19 -0500

    Fix teleporter

--- unity3d/Assets/Scrips/teleporter.cs
+++ unity3d/Assets/Scrips/teleporter.cs
@@ -10,44 +10,56 @@ public class teleporter : MonoBehaviour {
     private bool isRingEnable = false;
     public Transform Player;
     public float RayLenght = 50;
-	// Use this for initialization
-
-	
-	// Update is called once per frame
+    private bool isTeleportEnable = false;
+    private bool wasButtonHold = false;
 	void Update () {
         Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;       
-       
-          
-        if (Physics.Raycast(ray, out hit, RayLenght))
+        RaycastHit hit;
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick) && !wasButtonHold)
         {
-            if (hit.collider.tag == "Ground")
-            {
-                if (!TeleportMarker.activeSelf)
-                    TeleportMarker.SetActive(true);
-                TeleportMarker.transform.position = hit.point;
-                if (Input.GetAxis(buttonTeleport) > 0)
-                {
-                    Teleport();
-                }
-            }
-            else
-            {
-                TeleportMarker.SetActive(false);
-            }
-                
-            
+          isTeleportEnable = !isTeleportEnable;
+          wasButtonHold = true;
+        }
+        if (!OVRInput.Get(OVRInput.Button.PrimaryThumbstick))
+        {
+          wasButtonHold = false;
+        }
+        if(isTeleportEnable) {
+          if (Physics.Raycast(ray, out hit, RayLenght))
+          {
+              if (hit.collider.tag == "Ground")
+              {
+                  if (!TeleportMarker.activeSelf)
+                      TeleportMarker.SetActive(true);
+                  TeleportMarker.transform.position = hit.point;
+                  if (OVRInput.Get(OVRInput.Button.SecondaryThumbstick))
+                  {
+                      Teleport();
+                      isTeleportEnable = false;
+                      wasButtonHold = false;
+                  }
+              }
+              else
+              {
+                  TeleportMarker.SetActive(false);
+              }
+          }
+          else
+          {
+              TeleportMarker.SetActive(false);
+          }
         }
         else
         {
             TeleportMarker.SetActive(false);
         }
-        
+
 	}
 
-    void Teleport()
-    { 
-        Vector3 markerPosition = TeleportMarker.transform.position;
-        Player.position = new Vector3(markerPosition.x, Player.position.y, markerPosition.z);
-    }
+  void Teleport()
+  {
+      Vector3 markerPosition = TeleportMarker.transform.position;
+      Player.position = new Vector3(markerPosition.x, Player.position.y, markerPosition.z);
+
+  }
 }
